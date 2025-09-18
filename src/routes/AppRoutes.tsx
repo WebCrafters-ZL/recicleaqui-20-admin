e2b0d4cba8bc5d7c40f9fe7cb45a5aa7062a2b22
Thiago Araujo <122986262+AraujoThiago0822@users.noreply.github.com>
2025-09-18 09:17:09 -0300

    feat: implementação da parte de faturamento

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -4,12 +4,21 @@ import Login from "../pages/Login";
 import Register from "../pages/Register";
 import HomeUsuario from "../pages/HomeUsuario";
 
+import Faturamento from "../pages/Faturamento";
+
+import Pedidos from "../pages/Pedidos";
+
 const AppRoutes: React.FC = () => (
   <Routes>
     <Route path="/" element={<Navigate to="/login" replace />} />
     <Route path="/login" element={<Login />} />
     <Route path="/cadastro" element={<Register />} />
-    <Route path="/homeusuario" element={<HomeUsuario />} />
+    <Route path="/homeusuario" element={<HomeUsuario />}>
+
+      <Route path="faturamento" element={<Faturamento />} />
+    
+      <Route path="pedidos" element={<Pedidos />} />
+    </Route>
     <Route path="*" element={<Navigate to="/login" replace />} />
   </Routes>
 );
